e5cbb179a66c8a46433fb15f69c4c6a5744ac06e
yury.klimchuk <hydro.yura@gmail.com>
2024-12-11 23:57:49 +0300

    init containers

--- server/src/test/java/com/hydroyura/prodms/archive/server/db/repository/UnitRepositoryImplTest.java
+++ server/src/test/java/com/hydroyura/prodms/archive/server/db/repository/UnitRepositoryImplTest.java
@@ -1,15 +1,53 @@
 package com.hydroyura.prodms.archive.server.db.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import lombok.RequiredArgsConstructor;
+import com.hydroyura.prodms.archive.server.db.EntityManagerProvider;
+import jakarta.persistence.Persistence;
+import java.util.List;
+import java.util.Map;
+import org.junit.ClassRule;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 
-@RequiredArgsConstructor
 class UnitRepositoryImplTest {
+
+    @ClassRule
+    public static PostgreSQLContainer TEST_DB_CONTAINER =
+        new PostgreSQLContainer(DockerImageName.parse("postgres:14"))
+            .withDatabaseName("test-archive")
+            .withPassword("test-pg-pwd")
+            .withUsername("test-pg-user");
+
+
+    @BeforeAll
+    static void startContainer() {
+        TEST_DB_CONTAINER.setPortBindings(List.of("5435:5432"));
+        TEST_DB_CONTAINER.start();
+    }
+
+    @AfterAll
+    static void closeContainer() {
+        TEST_DB_CONTAINER.close();
+    }
+
+
+    private final EntityManagerProvider entityManagerProvider;
+
+
+    UnitRepositoryImplTest() {
+        // TODO uatoclosable ????
+        var entitymanagerFactory = Persistence.createEntityManagerFactory("test-archive-db");
+        this.entityManagerProvider = new EntityManagerProvider(entitymanagerFactory);
+    }
+
+
+    @Test
+    void test1() {
+        int a = 1;
+    }
 
 }
