d7e93388f1b7ffbbb029df5ddfc385a7c17f0794
yury.klimchuk <hydro.yura@gmail.com>
2025-01-05 00:48:55 +0300

    repaired ValidationManagerTest.java

--- server/src/test/java/com/hydroyura/prodms/archive/server/validation/ValidationManagerTest.java
+++ server/src/test/java/com/hydroyura/prodms/archive/server/validation/ValidationManagerTest.java
@@ -1,54 +1,53 @@
 package com.hydroyura.prodms.archive.server.validation;
 
 import com.hydroyura.prodms.archive.server.exception.model.ValidationException;
-import org.junit.jupiter.api.Test;
-import org.springframework.validation.Validator;
-
-import java.util.Collection;
+import com.hydroyura.prodms.archive.server.props.ValidationProps;
 import java.util.Collections;
+import org.junit.jupiter.api.Test;
+
 import java.util.List;
 import java.util.Map;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static com.hydroyura.prodms.archive.server.validation.ValidationManagerUtils.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-
+@ExtendWith(MockitoExtension.class)
 class ValidationManagerTest {
 
-    //private final ValidationManager validationManager;
+    private final ValidationManager validationManager;
 
-//    ValidationManagerTest() {
-//        var validator = Validator.forType(TestObjectForValidation.class, (obj, errs) -> {
-//            if (obj.getAge() ==  null || obj.getAge() < AGE) {
-//                errs.reject(ERROR_CODE_AGE_GREATER);
-//            }
-//        });
-//
-//        Map<Class<?>, List<AbstractValidator<?>>> validators = Map.of(TestObjectForValidation.class, List.of(validator));
-//        this.validationManager = new ValidationManager(validators, null);
-//    }
-//
-//
-//    @Test
-//    void validationSuccessTest() {
-//        var target = new TestObjectForValidation();
-//        target.setAge(AGE + 10);
-//        target.setName(NAME);
-//        assertDoesNotThrow(() -> validationManager.validate(target, TestObjectForValidation.class));
-//    }
-//
-//    @Test
-//    void validationFailedTest() {
-//        var target = new TestObjectForValidation();
-//        target.setAge(AGE - 5);
-//        target.setName(NAME);
-//        assertThrows(ValidationException.class, () -> validationManager.validate(target, TestObjectForValidation.class));
-//    }
-//
-//    @Test
-//    void notFoundValidatorsTest() {
-//        var target = Collections.EMPTY_MAP;
-//        //TODO: fix this test, e.g. skyMock method call
-//        //assertThrows(RuntimeException.class, () -> validationManager.validate(target, Map.class));
-//    }
+    ValidationManagerTest() {
+        var props = new ValidationProps();
+        props.setEnabled(Boolean.TRUE);
+        Map<Class<?>, List<AbstractValidator<?>>> validators = Map.of(
+            TestObjectForValidation.class, List.of(new TestValidator(props))
+        );
+        this.validationManager = Mockito.spy(new ValidationManager(validators));
+    }
+
+
+    @Test
+    void validationSuccessTest() {
+        var target = new TestObjectForValidation();
+        target.setAge(AGE + 10);
+        target.setName(NAME);
+        assertDoesNotThrow(() -> validationManager.validate(target, TestObjectForValidation.class));
+    }
+
+    @Test
+    void validationFailedTest() {
+        var target = new TestObjectForValidation();
+        target.setAge(AGE - 5);
+        target.setName(NAME);
+        assertThrows(ValidationException.class, () -> validationManager.validate(target, TestObjectForValidation.class));
+    }
+
+    @Test
+    void notFoundValidatorsTest() {
+        // TODO: create test for missing validator for certain types
+    }
+
 }
