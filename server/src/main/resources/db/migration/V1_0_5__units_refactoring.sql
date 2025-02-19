6a2087a6a838553222b86c824fd6eac80ceb0b18
YuryKlimchuk <hydro.yura@gmail.com>
2025-02-19 22:15:59 +0300

    Feature/init data script (#29)
    * added script
    
    * updated helm & fixed 1.0.5 migration & added docker compose for local test

--- server/src/main/resources/db/migration/V1_0_5__units_refactoring.sql
+++ server/src/main/resources/db/migration/V1_0_5__units_refactoring.sql
@@ -1,6 +1,6 @@
 -- Added parent constraint (need for getting drawing for -01; -02; e.g.)
 ALTER TABLE units
-    ADD COLUMN parent_number;
+    ADD COLUMN parent_number VARCHAR(50);
 ALTER TABLE units
     ADD CONSTRAINT fk_unit_parent
-        FOREIGN KEY (parent_number) units(number);
+        FOREIGN KEY (parent_number) REFERENCES units(number);
